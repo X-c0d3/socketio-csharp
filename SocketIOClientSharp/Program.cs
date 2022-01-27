5157951d7e3685a7e92bcdb8b4275d35317f79bf
X-c0d3 <giffyp@hotmail.com>
2022-01-27 20:39:16 +0700

    update

--- SocketIOClientSharp/Program.cs
+++ SocketIOClientSharp/Program.cs
@@ -1,82 +1,86 @@
-﻿using System;
-using System.Reactive.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging.Abstractions;
-using Socket.Io.Client.Core;
-using Socket.Io.Client.Core.Model;
-using Socket.Io.Client.Core.Model.SocketIo;
+﻿using System.Threading.Tasks;
+using System;
+using SocketIOClient;
+using System.Collections.Generic;
+using System.Net.Http;
 
-namespace SocketIOCleintSharp
-{
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            //optionally supply your own implementation of ILogger (default is NullLogger)
-            var config = new SocketIoClientOptions().With(logger: new NullLogger<SocketIoClient>());
-            using var client = new SocketIoClient(config);
-
-            //no need to hold reference to subscription as it
-            //will be automatically unsubscribed once client is disposed
-            client.Events.OnOpen.Subscribe((_) =>
-            {
-                Console.WriteLine("Socket has been opened");
+namespace SocketIOCleintSharp {
+    class Program {
+        static async Task Main(string[] args) {
+            // dont use /socket.io/  or /?EIO in url 
+            var socket = new SocketIO(new Uri("https://192.168.137.35:8000/acid"), new SocketIOOptions {
+                Query = new Dictionary<string, string>
+                {
+                    {"token", "V2" }
+                },
+                Transport = SocketIOClient.Transport.TransportProtocol.Polling,
+                EIO = 3,
+                //Path = "/acid"
             });
 
 
+            socket.HttpClient = new HttpClient(new HttpClientHandler {
+                ClientCertificateOptions = ClientCertificateOption.Manual,
+                ServerCertificateCustomValidationCallback = (httpRequestMessage, cert, cetChain, policyErrors) => true
+            });
 
-            //Emit message to server
+            socket.OnConnected += Socket_OnConnected;
+            socket.OnPing += (sender, e) => Console.WriteLine(" Ping");
+            socket.OnPong += (sender, e) => Console.WriteLine(" Pong: " + e.TotalMilliseconds);
+            socket.OnDisconnected += (sender, e) => Console.WriteLine(" disconnect: " + e);
+            socket.OnReconnectAttempt += (sender, e) => Console.WriteLine($"{DateTime.Now} Reconnecting: attempt = {e}");
+            socket.OnAny((name, response) => {
+                Console.WriteLine(name);
+                Console.WriteLine(response);
+            });
 
-    
-            if(client.State == ReadyState.Open)
-            {
-                //client.Emit("ESP", new { data = "some-data" }); //object data
-                await client.Emit("ESP", "some-data"); //string data
+            socket.On("planningstock", response => {
+                // Console.WriteLine(response.ToString());
+                Console.WriteLine(" =======> 1 " + response.GetValue<string>());
+            });
 
-                //with callback (acknowledgement)
-                //it is always called only once, no need to unsubscribe/dispose
-               client.Emit("ESP", "some-data").Subscribe(ack =>
-                {
-                    Console.WriteLine($"Callback with data: {ack.Data[0]}.");
-                });
+            socket.On("watchlist-room", response => {
+                // Console.WriteLine(response.ToString());
+                Console.WriteLine(" =======> 2 " + response.GetValue<string>());
+            });
+
+            socket.On("reflux-subscribe", response => {
+                // Console.WriteLine(response.ToString());
+                Console.WriteLine(" =======> 3 " + response.GetValue<string>());
+            });
+
+            socket.On("room", response => {
+                // Console.WriteLine(response.ToString());
+                Console.WriteLine(" =======> 4 " + response.GetValue<string>());
+            });
+
+
+            socket.On("ESP", response => {
+                // Console.WriteLine(response.ToString());
+                Console.WriteLine(" =======> 5 " + response.ToString());
+            });
+
+            await socket.ConnectAsync();
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        private static async void Socket_OnConnected(object sender, EventArgs e) {
+            Console.WriteLine("Socket_OnConnected");
+            var socket = sender as SocketIO;
+            Console.WriteLine("Socket.Id:" + socket.Id);
+
+            while (true) {
+                await Task.Delay(1000);
+
+                var dto = new { Id = 123, Name = "bob" };
+                if (socket.Connected)
+                    await socket.EmitAsync("ESP", dto);
+
             }
-
-
-
-
-            //Acknowledgements
-            client.On("ESP").Subscribe(e =>
-            {
-                //always check before calling acknowledgement
-                if (e.SupportsAcknowledgement)
-                {
-                    //without any data
-
-                    e.Acknowledge();
-
-                    //OR with any optional serializable data
-                    e.Acknowledge("message has been processed");
-                }
-            });
-
-
-            //subscribe to event with data
-            client.Events.OnPacket.Subscribe(packet =>
-            {
-                Console.WriteLine($"Received packet: {packet}");
-            });
-
-
-            //var options = new SocketIoOpenOptions("custom-path");
-            //await client.OpenAsync(new Uri("http://192.168.137.102:4000"), options);
-
-
-            await client.OpenAsync(new Uri("http://192.168.137.102:4000"));
-            Console.ReadLine();
         }
     }
 }
 
-//https://github.com/LadislavBohm/socket.io-client-core
-//PM> Install-Package Socket.Io.Client.Core
 
